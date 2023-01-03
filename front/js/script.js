825703af2c286ea1cbcdb0660e046b93bdacac46
malonetheressa <theressadmalone@gmail.com>
2023-01-03 19:49:16 +0100

    :construction: retrieving objects from api

--- front/js/script.js
+++ front/js/script.js
@@ -1,116 +1,51 @@
-const data = [
-  {
-    colors: ["Blue", "White", "Black"],
-    _id: "107fb5b75607497b96722bda5b504926",
-    name: "Kanap Sinopé",
-    price: 1849,
-    imageUrl: "http://localhost:3000/images/kanap01.jpeg",
-    description:
-      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    altTxt: "Photo of a blue sofa, two seats",
-  },
-  {
-    colors: ["Black/Yellow", "Black/Red"],
-    _id: "415b7cacb65d43b2b5c1ff70f3393ad1",
-    name: "Kanap Cyllène",
-    price: 4499,
-    imageUrl: "http://localhost:3000/images/kanap02.jpeg",
-    description:
-      "Morbi nec erat aliquam, sagittis urna non, laoreet justo. Etiam sit amet interdum diam, at accumsan lectus.",
-    altTxt: "Photo of a yellow and black sofa, four seats",
-  },
-  {
-    colors: ["Green", "Red", "Orange"],
-    _id: "055743915a544fde83cfdfc904935ee7",
-    name: "Kanap Calycé",
-    price: 3199,
-    imageUrl: "http://localhost:3000/images/kanap03.jpeg",
-    description:
-      "Pellentesque fermentum arcu venenatis ex sagittis accumsan. Vivamus lacinia fermentum tortor.Mauris imperdiet tellus ante.",
-    altTxt: "Photo of a green angle sofa, three seats",
-  },
-  {
-    colors: ["Pink", "White"],
-    _id: "a557292fe5814ea2b15c6ef4bd73ed83",
-    name: "Kanap Autonoé",
-    price: 1499,
-    imageUrl: "http://localhost:3000/images/kanap04.jpeg",
-    description:
-      "Donec mattis nisl tortor, nec blandit sapien fermentum at. Proin hendrerit efficitur fringilla. Lorem ipsum dolor sit amet.",
-    altTxt: "Photo of a pink sofa, one to two seats",
-  },
-  {
-    colors: ["Grey", "Purple", "Blue"],
-    _id: "8906dfda133f4c20a9d0e34f18adcf06",
-    name: "Kanap Eurydomé",
-    price: 2249,
-    imageUrl: "http://localhost:3000/images/kanap05.jpeg",
-    description:
-      "Ut laoreet vulputate neque in commodo. Suspendisse maximus quis erat in sagittis. Donec hendrerit purus at congue aliquam.",
-    altTxt: "Photo of a gray sofa, three seats",
-  },
-  {
-    colors: ["Grey", "Navy"],
-    _id: "77711f0e466b4ddf953f677d30b0efc9",
-    name: "Kanap Hélicé",
-    price: 999,
-    imageUrl: "http://localhost:3000/images/kanap06.jpeg",
-    description:
-      "Curabitur vel augue sit amet arcu aliquet interdum. Integer vel quam mi. Morbi nec vehicula mi, sit amet vestibulum.",
-    altTxt: "Photo of a gray sofa, two seats",
-  },
-  {
-    colors: ["Red", "Silver"],
-    _id: "034707184e8e4eefb46400b5a3774b5f",
-    name: "Kanap Thyoné",
-    price: 1999,
-    imageUrl: "http://localhost:3000/images/kanap07.jpeg",
-    description:
-      "EMauris imperdiet tellus ante, sit amet pretium turpis molestie eu. Vestibulum et egestas eros. Vestibulum non lacus orci.",
-    altTxt: "Photo of a red sofa, two seats",
-  },
-  {
-    colors: ["Pink", "Brown", "Yellow", "White"],
-    _id: "a6ec5b49bd164d7fbe10f37b6363f9fb",
-    name: "Kanap orthosie",
-    price: 3999,
-    imageUrl: "http://localhost:3000/images/kanap08.jpeg",
-    description:
-      "Mauris molestie laoreet finibus. Aenean scelerisque convallis lacus at dapibus. Morbi imperdiet enim metus rhoncus.",
-    altTxt: "Photo of a pink sofa, three seats",
-  },
-];
+// Fetch JSON array by making HTTP GET request to API endpoint
+fetch("http://localhost:3000/api/products")
+  .then((data) => {
+    return data.json(); // An array of objects turned into a format readable by JS using .json method
+  })
+  .then((products) => {
+    insertProducts(products);
+  });
+// This is a succession of functions. When using the 'then' method, the output of the first function becomes the input of the second.
+// Therefore (products), the input of the second function, is = to (data.json), the output of the first function.
+// The Fetch function returns a promise that resolves with a Response object when the request is successful.
+// The 'then' method is called on the returned promise and is used to specify what should be done with the response.
+// In this case, the first then block calls the json method on the Response object to parse the body of the response as JSON.
+// The second then block calls the insertProducts function and passes the parsed JSON data (the array of products) as an argument.
 
+// Get existing DOM element where I will insert product cards
 const productsContainer = document.getElementById("items");
 
-// array method
-data.forEach((product) => {
-  // step 1: create a link element w/ [href]
-  const productLink = document.createElement("a");
-  productLink.setAttribute("href", product._id);
-  // step 2: create a container article for image, title, and description
-  const containerArticle = document.createElement("article");
-  // step 3: create an image element w/ [alt]
-  const productImg = document.createElement("img");
-  productImg.src = product.imageUrl;
-  productImg.setAttribute("alt", product.altTxt);
-  // step 4: create a heading element w/ a class
-  const productHeading = document.createElement("h3");
-  productHeading.classList.add("productName");
-  productHeading.textContent = product.name;
-  // step 5: create a paragraph element w/ a class
-  const productDescription = document.createElement("p");
-  productDescription.classList.add("productDescription");
-  productDescription.textContent = product.description;
-  console.log(productDescription);
-  // step 6: append image to article
-  containerArticle.appendChild(productImg);
-  // step 7: append heading to article
-  containerArticle.appendChild(productHeading);
-  // step 8: append paragraph to article
-  containerArticle.appendChild(productDescription);
-  // step 9: append div to link
-  productLink.appendChild(containerArticle);
-  // step 10: append link to product container
-  productsContainer.appendChild(productLink);
-});
+// Iterate over each element in the JSON array from API
+function insertProducts(products) {
+  // Get the current product
+  products.forEach((product) => {
+    // Create a link element
+    const productLink = document.createElement("a");
+    productLink.setAttribute("href", product._id);
+    // Create a new DOM card element using article tag that will be inserted into home page
+    const containerArticle = document.createElement("article");
+    // Create an image element w/ [alt]
+    const productImg = document.createElement("img");
+    productImg.src = product.imageUrl;
+    productImg.setAttribute("alt", product.altTxt);
+    // Create a heading element w/ class
+    const productHeading = document.createElement("h3");
+    productHeading.classList.add("productName");
+    productHeading.textContent = product.name;
+    // Create a paragraph element w/ class
+    const productDescription = document.createElement("p");
+    productDescription.classList.add("productDescription");
+    productDescription.textContent = product.description;
+    // Append img to article
+    containerArticle.appendChild(productImg);
+    // Append heading to article
+    containerArticle.appendChild(productHeading);
+    // Append paragraph to article
+    containerArticle.appendChild(productDescription);
+    // Append div to link
+    productLink.appendChild(containerArticle);
+    // Append link to product container
+    productsContainer.appendChild(productLink);
+  });
+}
