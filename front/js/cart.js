e7056588278bf3e28f795b4f0a9f7368e6c2839c
malonetheressa <theressadmalone@gmail.com>
2023-01-12 17:54:02 +0100

    :construction:

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,5 +1,5 @@
 // TODO: [X] get cart data from localStorage
-// TODO: [] turn the JSON into JS obj array
+// TODO: [X] turn the JSON into JS obj array
 // TODO: [] create functions that iterate over the data for loops to build and append cards to the DOM
 // TODO: [] each cart item must have product, option, quantity, remove btn
 // TODO: [] contact form gathers customer info
@@ -7,3 +7,42 @@
 
 // Local storage
 const localStorageCart = JSON.parse(localStorage.getItem("cart"));
+console.table(localStorageCart); // logs JSON object
+
+// Build and append product cards to DOM
+function insertProducts() {
+  let cartContainer = document.getElementById("cart__items"); // Get existing DOM element where I will insert products
+
+  for (let product in localStorageCart) {
+    // Adding <article>
+    const productArticle = document.createElement("article");
+    productArticle.classList.add("cart__item");
+    productArticle.setAttribute(
+      "data-id",
+      localStorageCart[cartItem].cartProductId
+    );
+    cartContainer.appendChild(productArticle);
+
+    // Adding <div>
+    const productImgDiv = document.createElement("div");
+    productImgDiv.classList.add("cart__item__img");
+    productArticle.appendChild(productImgDiv);
+
+    // Adding <img>
+    const productCartImg = document.createElement("img");
+    // productCartImg.src = localStorageCart[cartItem].cartProductId
+    // productCartImg.setAttribute("alt", //);
+    productImgDiv.appendChild(productCartImg);
+
+    // Adding <div>
+    // Adding <h2>
+    // Adding <p>
+    // Adding <p>
+    // Adding <div>
+    // Adding <div>
+    // Adding <p>
+    // Adding <input>
+    // Adding <div>
+    // Adding <p>
+  }
+}
