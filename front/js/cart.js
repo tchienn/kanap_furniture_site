31f2602a34e4e9dd3302639a5d3f486471251446
malonetheressa <theressadmalone@gmail.com>
2023-01-12 16:55:10 +0100

    :construction: got cart from localStorage cart.js

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,6 +1,9 @@
-// TODO: get cart data from localStorage
-// TODO: turn the JSON into JS obj array
-// TODO: create functions that iterate over the data for loops to build and append cards to the DOM
-// TODO: each cart item must have product, option, quantity, remove btn
-// TODO: contact form gathers customer info
-// TODO: order now btn has eventListener that gets data, creates request body, POSTs to server, on success handle response and navigate to order confirmation page - window.location
+// TODO: [X] get cart data from localStorage
+// TODO: [] turn the JSON into JS obj array
+// TODO: [] create functions that iterate over the data for loops to build and append cards to the DOM
+// TODO: [] each cart item must have product, option, quantity, remove btn
+// TODO: [] contact form gathers customer info
+// TODO: [] order now btn has eventListener that gets data, creates request body, POSTs to server, on success handle response and navigate to order confirmation page - window.location
+
+// Local storage
+const localStorageCart = JSON.parse(localStorage.getItem("cart"));
