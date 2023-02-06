35a26bcb7d42b2ffb924ebbf741c4d8e951a3da0
malonetheressa <theressadmalone@gmail.com>
2023-02-06 13:50:43 +0100

    :sparkles: added instructions to confirmation.js

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,4 +1,9 @@
-// TODO: if POST request on cart page is successful server will send a response object with a confirmation number and the data you POSTed
-// TODO: pick a strategy to save data and send to this page - for example, localStorage, template literals
-// TODO: use data to build page programmatically
-// TODO: clear cart from localStorage once page is built (because contents of cart is already ordered)
+// TODO: [] if POST request on cart page is successful server will send a response object with a confirmation number and the data you POSTed
+// TODO: [] pick a strategy to save data and send to this page - for example, localStorage, template literals
+
+// URLSearchParams to retrieve order number
+const queryString = window.location.search; // Returns the entire parameter sting
+const urlParams = new URLSearchParams(queryString);
+
+// Post order number
+const productId = urlParams.get("id"); // Get product ID from string
--- front/js/product.js
+++ front/js/product.js
@@ -1,6 +1,6 @@
 // URLSearchParams
-const queryString = window.location.search; // Returns the entire parameter sting ?id=415b7cacb65d43b2b5c1ff70f3393ad1
-const urlParams = new URLSearchParams(queryString); // An object that allows me to manipulate a string using methods
+const queryString = window.location.search; // Returns the entire parameter sting
+const urlParams = new URLSearchParams(queryString);
 const productId = urlParams.get("id"); // Get product ID from string
 
 const uri = "http://localhost:3000/api/products/" + productId; // Gets relevant URL with product ID
