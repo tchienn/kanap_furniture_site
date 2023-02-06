e954e62399a858c96f2a2c39d998184b5ba5108c
malonetheressa <theressadmalone@gmail.com>
2023-02-06 22:45:10 +0100

    :bug: confirmation.js fix

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,5 +1,3 @@
-// TODO: [] if POST request on cart page is successful server will send a response object with a confirmation number and the data you POSTed
-
 // URLSearchParams to retrieve order number
 const queryString = window.location.search; // Returns the entire parameter sting
 const urlParams = new URLSearchParams(queryString);
