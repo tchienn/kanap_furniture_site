b8712c96f39fd8385aba12afefc5ab5835b9e040
malonetheressa <theressadmalone@gmail.com>
2023-02-21 14:31:53 +0100

    :art: updated product cards w price

--- front/js/script.js
+++ front/js/script.js
@@ -1,43 +1,49 @@
 // Imports function and calls it
-import { updateCartCount } from "./fun.js";
+import { updateCartCount } from './fun.js';
 updateCartCount();
 
 // Fetch JSON array by making HTTP GET request to API endpoint
-const uri = "http://localhost:3000/api/products";
+const uri = 'http://localhost:3000/api/products';
 
 fetch(uri)
-  .then((res) => {
-    return res.json(); //
-  })
-  .then((products) => {
-    insertProducts(products);
-  })
-  .catch((err) => console.error(err));
+    .then((res) => {
+        return res.json(); //
+    })
+    .then((products) => {
+        insertProducts(products);
+    })
+    .catch((err) => console.error(err));
 
 // Function iterates over each element in the JSON array retrieved from API and adds information to DOM
 function insertProducts(products) {
-  const productsContainer = document.getElementById("items"); // Get existing DOM element where I will insert product cards
-  products.forEach((product) => {
-    const productLink = document.createElement("a");
-    productLink.setAttribute("href", `./product.html?id=${product._id}`); // Get ID that links product on homepage to the same product on product page
-    productsContainer.appendChild(productLink);
+    const productsContainer = document.getElementById('items'); // Get existing DOM element where I will insert product cards
+    products.forEach((product) => {
+        const productLink = document.createElement('a');
+        productLink.setAttribute('href', `./product.html?id=${product._id}`); // Get ID that links product on homepage to the same product on product page
+        productsContainer.appendChild(productLink);
 
-    const productCard = document.createElement("article"); // Create a new DOM card element that will be inserted into home page
-    productLink.appendChild(productCard);
+        const productCard = document.createElement('article'); // Create a new DOM card element that will be inserted into home page
+        productLink.appendChild(productCard);
 
-    const productImg = document.createElement("img");
-    productImg.src = product.imageUrl;
-    productImg.setAttribute("alt", product.altTxt);
-    productCard.appendChild(productImg);
+        const productImg = document.createElement('img');
+        productImg.src = product.imageUrl;
+        productImg.setAttribute('alt', product.altTxt);
+        productCard.appendChild(productImg);
 
-    const productHeading = document.createElement("h3");
-    productHeading.classList.add("productName");
-    productHeading.textContent = product.name;
-    productCard.appendChild(productHeading);
+        const productHeading = document.createElement('h3');
+        productHeading.classList.add('productName');
+        productHeading.textContent = product.name;
+        productCard.appendChild(productHeading);
 
-    const productDescription = document.createElement("p");
-    productDescription.classList.add("productDescription");
-    productDescription.textContent = product.description;
-    productCard.appendChild(productDescription);
-  });
+        const productDescription = document.createElement('p');
+        productDescription.classList.add('productDescription');
+        productDescription.textContent = product.description;
+        productCard.appendChild(productDescription);
+
+        const productPrice = document.createElement('p');
+        productPrice.classList.add('productPrice');
+        productPrice.textContent = product.price;
+        productPrice.style.fontWeight = '700';
+        productCard.appendChild(productPrice);
+    });
 }
